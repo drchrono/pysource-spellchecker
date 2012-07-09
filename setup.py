461bcd64b66e30a4fd256256a0524c70c12f397c
Hartmut Goebel <h.goebel@goebel-consult.de>
2012-07-09 10:22:37 +0200

    Bump revision to 0.1.2.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pysource-spellchecker",
-    version = "0.1.1",
+    version = "0.1.2",
     scripts = ['pysource-spellchecker'],
     install_requires = ['pyenchant'],
 
